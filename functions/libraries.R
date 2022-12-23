8d536b868785898ca055da3a9e482a64a204602e
greenturtle135 <117742609+greenturtle135@users.noreply.github.com>
2022-12-23 17:31:55 +0400

    Add files via upload

---
+++ functions/libraries.R
@@ -0,0 +1,13 @@
+
+# If libraries are not downloaded on your computer, use install.packages("...") to install them to your computer.
+
+library(palmerpenguins)
+library(ggplot2)
+suppressPackageStartupMessages(library(janitor))
+suppressPackageStartupMessages(library(dplyr))
+library(tidyr)
+library(ragg)
+library(svglite)
+library(tidyverse)
+library(SciViews)
+library(ggpmisc)
